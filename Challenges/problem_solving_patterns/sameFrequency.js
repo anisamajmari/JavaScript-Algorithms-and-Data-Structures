8638aa0e28d0a736feddb1f71824408ff2a9a1bb
Anisa Majmari <anisamajmari@gmail.com>
2023-11-10 12:49:31 +0100

    Frequency counter

--- Challenges/problem_solving_patterns/sameFrequency.js
+++ Challenges/problem_solving_patterns/sameFrequency.js
@@ -1,1 +1,24 @@
-function sameFrequency() {}
+function sameFrequency(int1, int2) {
+  let str1 = int1.toString();
+  let str2 = int2.toString();
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < str1.length; i++) {
+    const ch = str1[i];
+    const index = str2.indexOf(ch);
+
+    if (index === -1) {
+      return false;
+    }
+    str2 = str2.replace(ch, "");
+  }
+
+  return true;
+}
+
+sameFrequency(182, 281); // true
+sameFrequency(34, 14); // false
+sameFrequency(3589578, 5879385); // true
+sameFrequency(22, 222); // false
